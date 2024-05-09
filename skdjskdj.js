4cf3787e191efadb8c987f9b802931fd99aa77c7
Ubejdullah Gashi <ubejd.b52@protonmail.com>
2024-05-10 00:28:16 +0200

    delete an unnecessary file "skdjskdj.js"

--- skdjskdj.js
+++
@@ -1,6 +0,0 @@
-fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        const ip = data.ip; // IP-Adresse des Nutzers
-        console.log(`Deine IP-Adresse ist: ${ip}`);
-    });
